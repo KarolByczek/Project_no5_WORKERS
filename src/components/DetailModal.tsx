670862af7e7f3b14e92bfc1a6e35bca9e14473b3
Karol Byczek <byczek_1986@interia.pl>
2024-01-23 23:18:03 +0100

    New styles for the DetailModal component.

--- src/components/DetailModal.tsx
+++ src/components/DetailModal.tsx
@@ -8,14 +8,16 @@ export const DetailModal = () => {
     const data:Employee = location.state;
 
   return (
-    <>
-    
-    <label htmlFor="lastname">Lastname:</label>
+    <div className='container'>
+      <div className=" mb-5 fw-bold fs-5">DETAILS:</div>
+      <div className="d-flex flex-column">
+        <label htmlFor="lastname">Lastname:</label>
     <input type="text" id="lastname" readOnly value={data.lastname} />
     <label htmlFor="fucker">Fucker:</label>
     <input type="text" id="fucker" readOnly value={`${data.fucker}`} />
     <label htmlFor="sucker">Sucker:</label>
     <input type="text" id="sucker" readOnly value={`${data.sucker}`} />
-    </>
+      </div>
+    </div>
   );
 };
