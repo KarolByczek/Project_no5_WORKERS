3cec581e006ab722cc9d1d0a96258bdeea5f86f6
Karol Byczek <byczek_1986@interia.pl>
2024-02-01 11:16:45 +0100

    Implementation of a function for formatting date in the DetailsModal page.

--- src/pages/DetailModal.tsx
+++ src/pages/DetailModal.tsx
@@ -1,12 +1,20 @@
 //import React from 'react'
 import { useLocation } from "react-router-dom";
 import { Employee } from "../components/Table";
-import '../index.css';
+import "../index.css";
 
 export const DetailModal = () => {
   const location = useLocation();
   console.log(location.state);
   const data: Employee = location.state;
+
+  const dateFormater = (date0: Date): string => {
+    const formatedmonth =
+      date0.getMonth() < 10 ? "0" + (date0.getMonth() + 1) : date0.getMonth();
+    const formatedday =
+      date0.getDate() < 10 ? "0" + date0.getDate() : date0.getDate();
+    return date0.getFullYear() + "-" + formatedmonth + "-" + formatedday;
+  };
 
   return (
     <div className="detail_container">
@@ -15,7 +23,12 @@ export const DetailModal = () => {
         <label htmlFor="lastname">Lastname:</label>
         <input type="text" id="lastname" readOnly value={data.lastname} />
         <label htmlFor="birthdate">Birthdate:</label>
-        <input type="text" id="birthdate" readOnly value={new Date(data.birthdate).toLocaleDateString()} />
+        <input
+          type="text"
+          id="birthdate"
+          readOnly
+          value={dateFormater(new Date(data.birthdate))}
+        />
         <label htmlFor="fucker">Fucker:</label>
         <input type="text" id="fucker" readOnly value={`${data.fucker}`} />
         <label htmlFor="sucker">Sucker:</label>
