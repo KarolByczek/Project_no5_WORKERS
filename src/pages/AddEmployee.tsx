8d59cc88229567700d4c3331bac1ce442fd70441
Karol Byczek <byczek_1986@interia.pl>
2024-02-02 11:52:39 +0100

    Final adjustments in the EditEmployee page.

--- src/pages/AddEmployee.tsx
+++ src/pages/AddEmployee.tsx
@@ -1,44 +1,55 @@
-import React from "react";
-import { Employee } from "../components/Table";
+import React, { useState } from "react";
+import { Employee, EmployeeStatus } from "../components/Table";
 import { useNavigate } from "react-router-dom";
+
+interface StatusOptions {
+  label: string;
+  value: string;
+}
 
 export function AddEmployee() {
   const navigate = useNavigate();
+  const [statusoptions] = useState<StatusOptions[]>([
+    { label: ":(", value: "weak" },
+    { label: ":/", value: "medium" },
+    { label: ":)", value: "strong" },
+  ]);
 
-  const makeEmployee = (formdata: FormData):Employee => {
+  const makeEmployee = (formdata: FormData): Employee => {
     return {
       id: Date.now().toString(),
-      firstname: formdata.get('firstname') as string,
-      lastname: formdata.get('lastname') as string,
-      birthdate: new Date(formdata.get('birthdate') as string),
-      salary: +(formdata.get('salary') as string),
-      fucker: formdata.get('fucker') as unknown as boolean,
-      status: 'weak',
-      sucker: formdata.get('sucker') as unknown as boolean
-    }
-  }
+      firstname: formdata.get("firstname") as string,
+      lastname: formdata.get("lastname") as string,
+      birthdate: new Date(formdata.get("birthdate") as string),
+      salary: +(formdata.get("salary") as string),
+      fucker: formdata.get("fucker") as unknown as boolean,
+      status: formdata.get("status") as EmployeeStatus,
+      sucker: formdata.get("sucker") as unknown as boolean,
+    };
+  };
 
   const handleAddEmployee = (event: React.FormEvent) => {
     event.preventDefault();
     //window.alert('Great Success! You have added your new employee to the list :)');
     const form = event.target as HTMLFormElement;
     const formdata = new FormData(form);
-    
-    const newEmployeeToSend = makeEmployee(formdata);
-    fetch('http://localhost:3000/employees', {
-      method: 'POST',
-      body: JSON.stringify(newEmployeeToSend)
-    }).then(response => {
-      if (response.status === 201) {
-        navigate('/');
-      } else {
-        console.log('Something went wrong!');
-        throw new Error('Mi scusi, tu es putana!');
-      }
-    }).catch(err => console.error(err))
 
-    console.log(newEmployeeToSend);
-    console.log('Formularz został wysłany')
+    const newEmployeeToSend = makeEmployee(formdata);
+    fetch("http://localhost:3000/employees", {
+      method: "POST",
+      body: JSON.stringify(newEmployeeToSend),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          navigate("/");
+        } else {
+          console.log("Something went wrong!");
+          throw new Error("Mi scusi, tu es putana!");
+        }
+      })
+      .catch((err) => console.error(err));
+
+    console.log("Formularz został wysłany");
   };
 
   return (
@@ -59,11 +70,21 @@ export function AddEmployee() {
         </label>
         <label htmlFor="status">
           Status:
-          <input name="status" type="text" />
+          <label htmlFor="status">
+            <select name="status">
+              {statusoptions.map((opt) => {
+                return (
+                  <option key={opt.value} value={opt.value}>
+                    {opt.label}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
         </label>
         <label htmlFor="birthdate">
           Birthdate:
-          <input name="birthdate" type="date" />
+          <input name="birthdate" type="text" />
         </label>
         <label htmlFor="fucker">
           Fucker:
--- src/pages/EditEmployee.tsx
+++ src/pages/EditEmployee.tsx
@@ -1,37 +1,57 @@
-import React from "react";
-import { Employee } from "../components/Table";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Employee, EmployeeStatus } from "../components/Table";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function EditEmployee() {
-
     const location = useLocation();
-    console.log(location.state);
+    const navigate = useNavigate()
     const data: Employee = location.state;
+    const [inputValue0, setInputValue0] = useState<any>(data.firstname);
+    const [inputValue1, setInputValue1] = useState<any>(data.lastname);
+    const [inputValue2, setInputValue2] = useState<any>(data.salary);
+    const [inputValue3, setInputValue3] = useState<any>(data.status);
+    const [inputValue4, setInputValue4] = useState<any>(new Date(data.birthdate).toDateString());
+    const [inputValue5, setInputValue5] = useState<any>(data.fucker);
+    const [inputValue6, setInputValue6] = useState<any>(data.sucker)
+    
 
   const makeEmployee = (formdata: FormData):Employee => {
     return {
-      id: +Date.now(),
+      id: Date.now().toString(),
       firstname: formdata.get('firstname') as string,
       lastname: formdata.get('lastname') as string,
       birthdate: new Date(formdata.get('birthdate') as string),
       salary: formdata.get('salary') as unknown as number,
       fucker: formdata.get('fucker') as unknown as boolean,
-      status: 'weak',
+      status: formdata.get("status") as EmployeeStatus,
       sucker: formdata.get('sucker') as unknown as boolean
     }
   }
 
   const handleEdit = (event: React.FormEvent) => {
     event.preventDefault();
-    //window.alert('Great Success! You have added your new employee to the list :)');
+    //window.alert('Great Success! You have edited your employee data:)');
     const form = event.target as HTMLFormElement;
     const formdata = new FormData(form);
-
     const editedData = makeEmployee(formdata);
-    console.log(editedData);
 
-    console.log('Formularz został wysłany')
+    fetch(`http://localhost:3000/employees/${data.id}`, {
+      method: "PUT",
+      body: JSON.stringify(editedData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          navigate("/");
+        } else {
+          console.log("Something went wrong!");
+          throw new Error("Mi scusi, tu es putana!");
+        }
+      })
+      .catch((err) => console.error(err));
+ 
+    console.log('The employee data has been edited')
   };
+
 
   return (
     <div>
@@ -39,31 +59,31 @@ export function EditEmployee() {
       <form className="add_employee_form" onSubmit={handleEdit}>
         <label htmlFor="firstname">
           First Name:
-          <input name="firstname" type="text" value={data.firstname} />
+          <input name="firstname" type="text" onChange={(event) => setInputValue0(event.target.value)} value={inputValue0} />
         </label>
         <label htmlFor="lastname">
           Last Name:
-          <input name="lastname" type="text" value={data.lastname} />
+          <input name="lastname" type="text" onChange={(event) => setInputValue1(event.target.value)} value={inputValue1} />
         </label>
         <label htmlFor="salary">
           Salary:
-          <input name="salary" type="number" value={data.salary} />
+          <input name="salary" type="number" onChange={(event) => setInputValue2(event.target.value)} value={inputValue2} />
         </label>
         <label htmlFor="status">
           Status:
-          <input name="status" type="text" value={data.status} />
+          <input name="status" type="text" onChange={(event) => setInputValue3(event.target.value)} value={inputValue3} />
         </label>
         <label htmlFor="birthdate">
           Birthdate:
-          <input name="birthdate" type="text" value={data.birthdate.toString()} />
+          <input name="birthdate" type="text" onChange={(event) => setInputValue4(event.target.value)} value={inputValue4} />
         </label>
         <label htmlFor="fucker">
           Fucker:
-          <input name="fucker" type="text" value={data.fucker.toString()} />
+          <input name="fucker" type="text" onChange={(event) => setInputValue5(event.target.value)} value={inputValue5} />
         </label>
         <label htmlFor="sucker">
           Sucker:
-          <input name="sucker" type="text" value={data.sucker.toString()} />
+          <input name="sucker" type="text" onChange={(event) => setInputValue6(event.target.value)} value={inputValue6} />
         </label>
         <button type="submit">SAVE AFTER EDITING HER OR HIM</button>
       </form>
