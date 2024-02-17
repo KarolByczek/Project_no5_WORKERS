8af53a1ab49805fd757f647687f3573f6ae0cd55
Karol Byczek <byczek_1986@interia.pl>
2024-02-17 12:36:33 +0100

    Appropriation of the QuestionModal styles.

--- src/components/QuestionModal.tsx
+++ src/components/QuestionModal.tsx
@@ -4,17 +4,15 @@ import { useTranslation } from "react-i18next";
 import { Employee } from "./Table";
 
 export const QuestionModal = (props: {
-  dataset: Employee[],
-  item: Employee,
-  className: string,
-  hook01: Function,
-  hook02: Function
+  dataset: Employee[];
+  item: Employee;
+  className: string;
+  hook01: Function;
+  hook02: Function;
 }) => {
-
   const { t } = useTranslation();
 
   const onClickButton01 = () => {
-
     fetch(`http://localhost:3000/employees/${props.item.id}`, {
       method: "DELETE",
       body: JSON.stringify(props.item),
@@ -30,19 +28,25 @@ export const QuestionModal = (props: {
       .then(() => console.log("The employee data has been deleted"))
       .catch((err) => console.error(err));
 
-    props.hook01(props.dataset.filter(guy => guy != props.item))
+    props.hook01(props.dataset.filter((guy) => guy != props.item));
     props.hook02(false);
   };
 
   return (
     <div className={props.className}>
+      {t("confirmation")}
       <div>
-        {t('confirmation')}
-        <button onClick={onClickButton01}>{t('yes')}</button>
+        {props.item.firstname} {props.item.lastname}, {t("birthdate")}: 
+        {props.item.birthdate.toString().substring(0, 10)}?
+      </div>
+      <div className="modbuttons">
+        <button onClick={onClickButton01}>{t("yes")}</button>
         <button
-          onClick={() => {props.hook02(false)}}
+          onClick={() => {
+            props.hook02(false);
+          }}
         >
-          {t('cancellation')}
+          {t("cancellation")}
         </button>
       </div>
     </div>
