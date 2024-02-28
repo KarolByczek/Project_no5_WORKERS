0177a1e432e28536135605b21a19f4596166a14a
KarolByczek <florianmusial86@gmail.com>
2024-02-28 14:46:15 +0100

    ...

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -23,7 +23,7 @@ export interface Employee {
   firstname: string,pppp
   lastname: string,
   salary: number,
-  status: EmployeeStatus,
+  status: EmployeeStatus,ppp
   birthdate: string,
   fucker: string,
   sucker: string
