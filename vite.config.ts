becaf2e5d6d625de6145e2ec50bf6d285c4223c9
KarolByczek <byczek_1986@interia.pl>
2024-09-04 11:24:12 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Project_no5_WORKERS/'
+  base: '/Project_no5_WORKERS/',
+  build: {
+    chunkSizeWarningLimit: 1000
+  },
 })
